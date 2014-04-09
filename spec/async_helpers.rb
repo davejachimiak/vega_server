fb2d26d3c2370ff464b5a1760a0725573149a487
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-04-09 17:12:01 -0400

    spec for bad origin

--- spec/async_helpers.rb
+++ spec/async_helpers.rb
@@ -22,7 +22,7 @@ VegaServer::HandshakeSteps = RSpec::EM.async_steps do
     EM.next_tick(&callback)
   end
 
-  def open_socket(&callback)
+  def open_socket(origin, &callback)
     done = false
 
     resume = lambda do |open|
@@ -35,12 +35,19 @@ VegaServer::HandshakeSteps = RSpec::EM.async_steps do
 
     @ws = Faye::WebSocket::Client.new('ws://0.0.0.0:9292')
 
+    @ws.instance_variable_set(:@headers, { 'HTTP_ORIGIN' => origin })
+
     @ws.on(:open) { |e| resume.call(true) }
     @ws.onclose = lambda { |e| resume.call(false) }
   end
 
   def assert_socket_open(&callback)
     expect(@open).to eq true
+    EM.next_tick(&callback)
+  end
+
+  def assert_socket_closed(&callback)
+    expect(@open).to eq false
     EM.next_tick(&callback)
   end
 end
--- spec/protocol/handshake_spec.rb
+++ spec/protocol/handshake_spec.rb
@@ -4,15 +4,30 @@ describe 'handshake' do
   include VegaServer::HandshakeSteps
 
   context 'allowed origins are configured' do
-    context 'origin of client is allowed' do
-      let(:origins) { ['http://www.example.org'] }
-      before { configure_origins(origins) }
+    let(:allowed_origins) { [allowed_origin] }
+    let(:allowed_origin) { 'http://www.example.org' }
 
+    before { configure_origins(allowed_origins) }
+
+    context 'origin of client is allowed' do
       it 'leaves the connection open' do
         start_server
-        open_socket
+        open_socket(allowed_origin)
 
         assert_socket_open
+
+        stop_server
+      end
+    end
+
+    context 'origin of client is not allowed' do
+      let(:bad_origin) { 'http://www.example.com' }
+
+      it 'closes the connection' do
+        start_server
+        open_socket(bad_origin)
+
+        assert_socket_closed
 
         stop_server
       end
